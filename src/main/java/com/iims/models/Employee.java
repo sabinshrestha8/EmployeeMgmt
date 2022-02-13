9c95b501081251dc296af224cf6fb1b7e6a7a858
Aayush <ayxsth@gmail.com>
2022-02-13 20:29:06 +0545

    added models

---
+++ src/main/java/com/iims/models/Employee.java
@@ -0,0 +1,101 @@
+package com.iims.models;
+
+public class Employee {
+    private int id;
+    private String name;
+    private long contact;
+    private String address;
+    private int age;
+    private String joinDate;
+    private int departmentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getContact() {
+        return contact;
+    }
+
+    public void setContact(long contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(String joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contact=" + contact +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", joinDate='" + joinDate + '\'' +
+                ", departmentId=" + departmentId +
+                '}';
+    }
+
+    public Employee() {}
+
+    public Employee(int id, String name, long contact, String address, int age, String joinDate, int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.address = address;
+        this.age = age;
+        this.joinDate = joinDate;
+        this.departmentId = departmentId;
+    }
+
+    public Employee(String name, long contact, String address, int age, String joinDate, int departmentId) {
+        this.name = name;
+        this.contact = contact;
+        this.address = address;
+        this.age = age;
+        this.joinDate = joinDate;
+        this.departmentId = departmentId;
+    }
+}
